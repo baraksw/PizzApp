de0e95104c6a427a1fc51c8671b963f4da793f84
tuval190 <57136782+tuval190@users.noreply.github.com>
2019-11-18 23:52:16 +0200

    Uploding the java and xml files for topping activity.

---
+++ app/src/main/java/com/example/pizzapp/Toppings.java
@@ -0,0 +1,14 @@
+package com.example.pizzapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Toppings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_toppings);
+    }
+}
