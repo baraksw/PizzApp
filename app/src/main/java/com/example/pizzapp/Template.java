587e37e184a53b8575c19efe083d2235e6d282fa
TOMERSHAHAF <57099421+TOMERSHAHAF@users.noreply.github.com>
2019-11-25 13:44:33 +0200

    making the toppings button working

--- app/src/main/java/com/example/pizzapp/Template.java
+++ app/src/main/java/com/example/pizzapp/Template.java
@@ -4,21 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Template extends AppCompatActivity {
 
-int a;
+    private ImageView mashrooms_image;
+    private boolean mashrooms_view=false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_template);
-        a=0;
+        mashrooms_image = findViewById(R.id.mashrooms);
     }
 
     public void showMashroom(View view) {
-        a=1;
-
+        if(!mashrooms_view) {
+            mashrooms_image.setVisibility(View.VISIBLE);
+            mashrooms_view = true;
+        }
+        else {
+            mashrooms_image.setVisibility(View.INVISIBLE);
+            mashrooms_view = false;
+        }
 
     }
 
