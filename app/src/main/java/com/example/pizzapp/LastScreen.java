8c390f3f7fd14205babd779703df8110a365025f
asafbek <asafbek1@gmail.com>
2019-12-09 03:12:23 +0200

    adding the shape xml and updates because tuval is stupid

--- app/src/main/java/com/example/pizzapp/LastScreen.java
+++ app/src/main/java/com/example/pizzapp/LastScreen.java
@@ -15,14 +15,6 @@ public class LastScreen extends AppCompatActivity {
         setContentView(R.layout.activity_last_screen);
         Intent intent = getIntent();
         String message = intent.getStringExtra(CustomerDetails.EXTRA_MESSAGE);
-        String howToPay;
-
-        howToPay = intent.getStringExtra(CustomerDetails.EXTRA_MESSAGE2);
-        howToPay = intent.getStringExtra(Credit.EXTRA_MESSAGE3);
-        TextView textView = findViewById(R.id.text_message);
-        TextView textView1 = findViewById(R.id.text_howToPay);
-        textView.setText(message);
-        textView1.setText(howToPay);
     }
 
     public void launchLastScreen(View view) {
